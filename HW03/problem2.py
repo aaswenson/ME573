607756c1b2d01ef0312e7b809b221f2c30b0ee1b
Alexander Swenson <aaswenson@wisc.edu>
2017-10-02 16:07:18 -0500

    clean up code, prep results for upload as zip file

--- HW03/problem2.py
+++ HW03/problem2.py
@@ -7,17 +7,20 @@ from problem1 import make_b_array
 from solvers import gaussian_elimination, thomas_solver
 
 def make_A_b(n):
-     e = np.ones((n, 1))
-     data = np.hstack((e, -2*e, e)).T
-     diags = np.array((-1, 0, 1))
+    """Produce sparse diagonal matrix for algorithm testing.
+    """
+    e = np.ones((n, 1))
+    data = np.hstack((e, -2*e, e)).T
+    diags = np.array((-1, 0, 1))
+    sp_array = scipy.sparse.spdiags(data, diags, n, n).toarray()
+    B = make_b_array(n)
 
-     sp_array = scipy.sparse.spdiags(data, diags, n, n).toarray()
-     
-     B = make_b_array(n)
-
-     return sp_array, B
+    return sp_array, B
 
 def scaling_analysis():
+    """Run the scaling analysis for comparison between Thomas and Gaussian
+    Elimination algorithms.
+    """
     gauss_ts = []
     thoms_ts = []
     n = []
@@ -31,7 +34,8 @@ def scaling_analysis():
     return [n, gauss_ts, thoms_ts]
 
 def plot_results(results):
-    
+    """Plot the results of the scaling analysis.
+    """
     plt.plot(results[0], results[1], label="gaussian")
     plt.plot(results[0], results[2], label="thomas")
     plt.title("Computation Time Gaussian vs. Thomas Algorithms")
@@ -40,9 +44,6 @@ def plot_results(results):
     plt.legend()
     plt.savefig("problem2.pdf")
 
-
-
 if __name__ == '__main__':
-    
     results = scaling_analysis()
     plot_results(results)
