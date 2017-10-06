running code, incorrect results

HW04/problem4.py
@@ -27,63 +27,95 @@ def get_f_f_prime(domain, dx):
     
     return [x, fx, f_prime]
 
-def forward_diff(fx, dx, n):
+def forward_diff(fx, dx):
     """Approximate f'(x) using forward difference numerical scheme.
     """
-    f_prime = np.zeros(fx.size)
-    for idx in range(0, fx.size - 1):
-        f_prime[idx] = (fx[idx + 1] - fx[idx]) / dx
+    f_prime = []
+    for idx in range(0, len(fx) - 1):
+        f_prime.append((fx[idx + 1] - fx[idx]) / dx)
+    
     return f_prime
 
-def second_order_cent(fx, dx, n):
+def second_order_cent(fx, dx):
     """Approximate f'(x) using 2nd order numerical scheme.
     """
-    f_prime = np.zeros(fx.size)
-    for idx in range(1, fx.size - 1):
-        f_prime[idx] = (fx[idx+1] - fx[idx-1]) / (2 * dx)
+    f_prime = []
+    for idx in range(1, len(fx) - 1):
+        f_prime.append((fx[idx+1] - fx[idx-1]) / (2 * dx))
     return f_prime
 
-def fourth_order_cent(fx, dx, n):
+def fourth_order_cent(fx, dx):
     """Approximate f'(x) using 4th order numerical scheme.
     """
-    f_prime = np.zeros(fx.size)
-    for idx in range(2, fx.size - 2):
-        f_prime[idx] = (fx[idx-2] - 8*fx[idx-1] + 8*fx[idx+1] - fx[idx+2]) /\
-                (12 * dx)
+    f_prime = []
+    for idx in range(2, len(fx) - 2):
+        f_prime.append((fx[idx-2] - 8*fx[idx-1] + 8*fx[idx+1] - fx[idx+2]) /\
+                (12 * dx))
     return f_prime
 
-def compute_num_error(approx, exact):
+def compute_num_error(approx, exact, stencil_idx=(None, None)):
     """Compute numerical error of f'(x) approximation.
     """
     
-    error = abs(approx - exact)/(sum(exact))
+    approx_array = np.array(approx)
+    exact_array = np.array(exact[stencil_idx[0]:stencil_idx[1]])
+
+    error = abs(np.subtract(approx_array, exact_array)) / sum(exact_array)
 
     return error
 
-def compare_numerical_schemes(domain):
+def compare_numerical_schemes(domain, dx):
     """Compare the numerical approximations of f'(x)
     """
-    dx = np.linspace(1e-2, 1e-5)
-    for i, dx in enumerate(dx):
-        n = (domain[1] - domain[0]) / dx
-        exact = get_f_f_prime(domain, dx)[2]
-        print exact
-        first_order = forward_diff(exact, dx, n)
-        second_order = second_order_cent(exact, dx, n)
-        fourth_order = fourth_order_cent(exact, dx, n)
-        
-        first_error = compute_num_error(first_order, exact)
-        second_error = compute_num_error(second_order, exact)
-        fourth_error = compute_num_error(fourth_order, exact)
+    x, fx, exact = get_f_f_prime(domain, dx)
+    er1 = np.zeros(len(exact))
+    er2 = np.zeros(len(exact))
+    er3 = np.zeros(len(exact))
+    # calculate approximations of f'(x)
+    first = forward_diff(fx, dx)
+    second = second_order_cent(fx, dx)
+    fourth = fourth_order_cent(fx, dx)
+    # calculate error of approximations
+    er1 = compute_num_error(first, exact, (0, -1))
+    er2 = compute_num_error(second, exact, (1, -1))
+    er4 = compute_num_error(fourth, exact, (2, -2))
 
-    return dx, [first_error, second_error, fourth_error]
+    return er1, er2, er4, x
+
+def error_decay(domain):
+    
+    er1 = []
+    er2 = []
+    er4 = []
+    dx = [0.0001, 0.0005, 0.001, 0.01, 0.05]
+    x_eval = 0.2
+    f_prime_exact = eval_f_prime(x_eval)
+    for i, h in enumerate(dx):
+        # get fx
+        x, fx, exact = get_f_f_prime(domain, h)
+        # get list index
+        idx = int((x_eval - domain[0]) / h)      
+        # calculate approximations of f'(x)
+        first = forward_diff(fx, h)[idx]
+        second = second_order_cent(fx, h)[idx]
+        fourth = fourth_order_cent(fx, h)[idx]
+        err1 = abs(first - f_prime_exact) / abs(f_prime_exact)
+        err2 = abs(second - f_prime_exact) / abs(f_prime_exact)
+        err4 = abs(fourth - f_prime_exact) / abs(f_prime_exact)
+
+        er1.append(err1)
+        er2.append(err2)
+        er4.append(err4)
+
+    return er1, er2, er4, dx
+
 
 # plotting functions follow this comment
 
 def plot_parta(data, title):
     """Plot f(x), f'(x) from part A
     """
-    plt.figure()
+    plt.figure(1)
     plt.semilogy(data[0], data[1], label='f(x)')
     plt.semilogy(data[0], data[2], label="f'(x)")
     plt.legend()
@@ -93,17 +125,28 @@ def plot_parta(data, title):
 #    plt.show()
 
 def plot_partb(dx, data, title):
-    plt.figure() 
-    plt.plot(dx, data[0])
-    plt.plot(dx, data[1])
-    plt.plot(dx, data[2])
+    plt.figure(2)
+    plt.plot(dx[:-1], data[0], label='1st Order')
+    plt.plot(dx[1:-1], data[1], label='2nd Order')
+    plt.plot(dx[2:-2], data[2], label='4th Order')
+    plt.legend()
+    plt.show()
+
+def plot_partc(h, data, title):
+    plt.figure(2)
+    plt.plot(h, data[0], label='1st Order')
+    plt.plot(h, data[1], label='2nd Order')
+    plt.plot(h, data[2], label='4th Order')
+    plt.legend()
     plt.show()
 
 if __name__ == '__main__':
     
-    domain = (0.1, 0.4)
-    dx = 0.0005
+    domain = (0.1, 1)
+    dx = 0.005
     data = get_f_f_prime(domain, dx)
-    plot_parta(data, 'Part A')
-    step_sizes, results = compare_numerical_schemes(domain)
-    plot_partb(step_sizes, results, 'Part B')
+#    plot_parta(data, 'Part A')
+    e1, e2, e3, x = compare_numerical_schemes(domain, dx)
+#    plot_partb(x, [e1, e2, e3], 'Part B')
+    er1, er2, er4, h_vals = error_decay(domain)
+    plot_partc(h_vals, [er1, er2, er4], 'Part C')