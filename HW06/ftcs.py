de97901605985cb530b6af24a22ec0b32f77bae2
Alexander Swenson <aaswenson@wisc.edu>
2017-10-26 16:25:29 -0500

    nearly working 1, need to fix the ftcs approx, oscillating too much

--- HW06/ftcs.py
+++ HW06/ftcs.py
@@ -4,7 +4,6 @@ import scipy.sparse
 import matplotlib.pyplot as plt
 
 from solvers import solve_matrix, gaussian_elimination, thomas_solver
-from ftcs import make_A_FTCS, make_B_FTCS
 
 # some constants
 k = 1e-3
@@ -17,11 +16,11 @@ n = int((domain[1] - domain[0]) / dx)
 x = np.linspace(domain[0], domain[1], n) 
 times = np.linspace(0, t_end, int(t_end / dt))
 
-def make_A_CN():
+def make_A_FTCS():
     """Produce sparse diagonal matrix for algorithm testing.
     """
     e = np.ones((n, 1))
-    data = np.hstack((-alpha*e, 2*(alpha+1)*e, -alpha*e)).T
+    data = np.hstack((alpha * e, (1 - 2*alpha)*e, alpha * e)).T
     diags = np.array((-1, 0, 1))
     sp_array = scipy.sparse.spdiags(data, diags, n, n).toarray()
     sp_array[0][0] = 1; sp_array[0][1:] = 0
@@ -29,15 +28,16 @@ def make_A_CN():
 
     return sp_array
 
-def make_B_CN(f):
+def make_B_FTCS(f):
     
-    b = np.zeros(n)
-    for i, val in enumerate(f[1:-1]):
-        b[i] = alpha*f[i-1] + 2*(1-alpha)*f[i] + alpha*f[i+1]
+    b = f
+    b[0] = 0
+    b[n-1] = 0
+     
     return b
 
 def get_analytic_solution(x, t):
-    fa = np.zeros(n)
+    fa = np.zeros(len(x))
     for i, x_val in enumerate(x):
         fa[i] = ( 1 ) * ( math.erf( (1-x_val) / (2 * math.sqrt(k * t))) - \
                 math.erf(-(1+x_val) / (2 * math.sqrt(k * t) )) )
@@ -45,11 +45,10 @@ def get_analytic_solution(x, t):
     return fa
 
 
-def plot(fig, data, savefig):
-    plt.figure(fig)
+def plot(data, savefig):
+
     plt.plot(data[0], data[1], label='Analytic')
     plt.plot(data[0], data[2], label='CN')
-    plt.plot(data[0], data[3], label='FTCS')
     plt.legend()
     plt.title("CN vs. FTCS dx = 0.05 dt = 2.0")
     plt.xlabel('x [-]')
@@ -57,27 +56,29 @@ def plot(fig, data, savefig):
     plt.savefig("{0}.png".format(savefig))
     plt.show()
 
-if __name__ == '__main__':
-    
-    fa = get_analytic_solution(x, t_end)
-    # Set Up CN Stuff
-    A = make_A_CN()
+def run_ftcs():
+    A = make_A_FTCS()
     f = np.piecewise(x, [abs(x) < 1], [2])
-    B = make_B_CN(f)
+    B = make_B_FTCS(f)
     f = solve_matrix(A, B)[0]
-    # Set up FTCS stuff
-    A_FTCS = make_A_FTCS()
-    f_FTCS = np.piecewise(x, [abs(x) < 1], [2])
-    B_FTCS = make_B_FTCS(f_FTCS)
-    f_FTCS = solve_matrix(A_FTCS, B_FTCS)[0]
 
     for i in range(0, len(times)):
-        # Sovle CN
         B = make_B_CN(f)
         f = solve_matrix(A, B)[0]
-        # Solve FTCS
-        f_FTCS = solve_matrix(A_FTCS, B_FTCS)[0]
-        B_FTCS = make_B_FTCS(f_FTCS)
 
-    data = [x, fa, f, f_FTCS]
-    plot(2, data, 'problem1.png')
+    return f
+
+if __name__ == '__main__':
+    
+    A = make_A_FTCS()
+    f = np.piecewise(x, [abs(x) < 1], [2])
+    B = make_B_FTCS(f)
+    f = solve_matrix(A, B)[0]
+
+    for i in range(0, len(times)):
+        B = make_B_FTCS(f)
+        f = solve_matrix(A, B)[0]
+
+    fa = get_analytic_solution(x, t_end)
+    data = [x, fa, f]
+    plot(data, 'problem1.png')
