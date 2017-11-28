7d2cdb647ec31a9c9716c681f342428d23497507
Alexander Swenson <aaswenson@wisc.edu>
2017-11-28 16:06:29 -0600

    finished HW10

--- HW10/problem2.m
+++ HW10/problem2.m
@@ -24,17 +24,10 @@ f_CN = f_ftcs;
 
 t = t_init;
 while t < t_final;
-
-    f_ftcs(2:N-1) = f_ftcs(2:N-1) - f_ftcs(2:N-1).*(dt/dx).*(f_ftcs(3:N) - f_ftcs(1:N-2))*0.5 + ...
-               alpha*(f_ftcs(3:N) - 2*f_ftcs(2:N-1) + f_ftcs(1:N-2));
-    f_ftcs(1) = analytic(x(1), L, t);
-    f_ftcs(N) = analytic(x(N), L, t);
+    f_ftcs = ftcs(x, t, f_ftcs, N, dt, dx, L);
+    f_CN = CN(x, t, f_CN, N, dt, dx, L);
     t = t + dt;
 end
-
-Co = abs(f_analytic * (dt / dx));
-Rec = Co / alpha;
-
 
 end
 
@@ -44,4 +37,45 @@ function f = analytic(x,L,t)
     eterm = exp(-kappa*t/(L*L));
     f = -(2*kappa/L) * cosh(x/L) ./ (sinh(x/L) + eterm);
 end
+
+function f_ftcs = ftcs(x, t, f_ftcs, N, dt, dx, L)
+    % execute one time step with FTCS scheme
+    global alpha
+    f_ftcs(2:N-1) = f_ftcs(2:N-1) - f_ftcs(2:N-1).*(dt/dx).*(f_ftcs(3:N) - f_ftcs(1:N-2))*0.5 + ...
+               alpha*(f_ftcs(3:N) - 2*f_ftcs(2:N-1) + f_ftcs(1:N-2));
+    f_ftcs(1) = analytic(x(1), L, t);
+    f_ftcs(N) = analytic(x(N), L, t);
+
+end
+
+function f_CN = CN(x, t, f_CN, N, dt, dx, L)
+    % Execute one time step with CN scheme
+    A = update_A(f_CN, N, dt, dx);
+    b = update_b(f_CN, N, dt, dx);
+    f_CN(2:N-1) = thomas(A,b(2:N-1));
+    f_CN(1) = analytic(x(1), L, t);
+    f_CN(N) = analytic(x(N), L, t);
+    
+end
+
+
+function A = update_A(f, N, dt, dx)
+    global alpha
+    e = ones(N-1, 1);
+    % set ghost point to make matrix
+    f(N+1) = 1;
+    A = spdiags([-((f(3:N+1)'*dt/dx) +2*alpha).*e, 4*(1+alpha)*e, (f(1:N-1)'*(dt/dx) - 2*alpha).*e], -1:1, N-2, N-2);
+end
+
+function b = update_b(f, N, dt, dx)
+    global alpha
+    b = ones(1,N);
+    b(2:N-1) = (f(2:N-1) * (dt/dx) + 2*alpha).*f(1:N-2) + ...
+                4*(1-alpha)*f(2:N-1) + ...
+               (2*alpha - f(2:N-1)*(dt/dx)).*f(3:N);
+    b(2) = b(2) + (f(2)*dt / dx + 2*alpha)*f(1);
+    b(N-1) = b(N-1) + (2*alpha - f(N-1)*dt / dx)*f(N);
+    
+end
+
 
--- HW10/run_script.m
+++ HW10/run_script.m
@@ -13,7 +13,7 @@ plot(p1x, p1ftbs, 'r', p1x, p1ftcs, 'g', p1x, p1CN, 'b', p1x, p1exact, '-o')
 title('Comparison of Linear Advection Schemes')
 xlabel('X [-]')
 ylabel('f(X) [-]')
-legend('FTBS', 'FTCS', 'CN','Exact')
+legend('FTBS', 'FTCS', 'CN', 'Exact')
 saveas(gcf,'./writeup/problem1.png')
 i = i + 1;
 
@@ -22,6 +22,15 @@ plot(p2x, p2ftcs, 'o', p2x, p2exact)
 title('Burgers Equation')
 xlabel('X [-]')
 ylabel('f(X) [-]')
-legend('FTCS','Exact')
+legend('FTCS', 'Exact')
 saveas(gcf,'./writeup/problem2_ftcs.png')
+i = i + 1;
+
+figure(start_plot + i)
+plot(p2x, p2CN, 'o', p2x, p2exact)
+title('Burgers Equation')
+xlabel('X [-]')
+ylabel('f(X) [-]')
+legend('Crank-Nicolson', 'Exact')
+saveas(gcf,'./writeup/problem2_CN.png')
 i = i + 1;
