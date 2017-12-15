800793e15b73af15b5b4dc7eb06d218eb9243492
Alexander Swenson <aaswenson@wisc.edu>
2017-12-15 11:09:58 -0600

    adding untracked files, fixing last commit

---
+++ HW12/advection_diffusion.m
@@ -0,0 +1,85 @@
+%% Alex Swenson ME573 HW10, Problem 1
+function [fuCN, fvCN] = advection_diffusion(dt, dx, dy, u, v, nu)
+global alphax alphay
+%% Problem Parameters
+
+nx = length(u(:,1));
+ny = length(v(1,:));
+alphax = nu*dt / dx^2; alphay = nu*dt / dy^2;
+
+fuCN = u;
+fvCN = v;
+fu_CN = u;
+fu_saveCN = u;
+fv_CN = v;    
+fv_saveCN = v;
+%% March in Time
+    for j=2:nx-1
+        % ----------------- Crank Nicholson ------------------- %
+        % calculate u direction
+        Aux = update_Ax(fuCN, nx, dt, dx, j);
+        bux = update_bx(fuCN, nx, dt, dx, j);
+        fu_CN(2:ny-1,j) = thomas(Aux, bux);
+        % calculate v direction
+        Avx = update_Ax(fvCN, nx, dt, dx, j);
+        bvx = update_bx(fvCN, nx, dt, dx, j);
+        fv_CN(2:ny-1,j) = thomas(Avx, bvx);
+    end
+    for i=2:ny-1
+        % ----------------- Crank Nicholson ------------------- %
+        % calculate u direction
+        Auy = update_Ay(fu_CN, ny, dt, dy, i);
+        buy= update_by(fu_CN, ny, dt, dy, i);
+        fu_saveCN(i,2:nx-1) = thomas(Auy, buy);
+        % calculate v direction
+        Avy = update_Ay(fv_CN, ny, dt, dy, i);
+        bvy= update_by(fv_CN, ny, dt, dy, i);
+        fv_saveCN(i,2:nx-1) = thomas(Avy, bvy);
+    end
+fuCN = fu_saveCN;
+fvCN = fv_saveCN;
+
+end
+
+function A = update_Ax(f, N, dt, dx, j)
+    global alphax
+    f = f(:,j)';
+    e = ones(N-1, 1);
+    % set ghost point to make matrix
+    f(N+1) = 1;
+    A = spdiags([-((f(3:N+1)'*dt/dx) +2*alphax).*e, 4*(1+alphax)*e, (f(1:N-1)'*(dt/dx) - 2*alphax).*e], -1:1, N-2, N-2);
+end
+
+function A = update_Ay(f, N, dt, dx, i)
+    global alphay
+    f = f(i,:);
+    e = ones(N-1, 1);
+    % set ghost point to make matrix
+    f(N+1) = 1;
+    A = spdiags([-((f(3:N+1)'*dt/dx) +2*alphay).*e, 4*(1+alphay)*e, (f(1:N-1)'*(dt/dx) - 2*alphay).*e], -1:1, N-2, N-2);
+end
+
+function b = update_bx(f, N, dt, dx, j)
+    % update the RHS vector, include initial boundary conditions for 
+    % first and last nodes.
+    global alphax
+    f = f(:,j)';
+    c = dt/dx;
+    b = (c*f(2:N-1)+2*alphax).*f(1:N-2) + 4*(1-alphax)*f(2:N-1) + (2*alphax-c*f(2:N-1)).*f(3:N);
+    b(1) = b(1) + (c*f(2)+2*alphax)*f(1);
+    b(N-2) = b(N-2) + (2*alphax - f(N-2)*c)*f(N);
+end
+
+function b = update_by(f, N, dt, dy, i)
+    % update the RHS vector, include initial boundary conditions for 
+    % first and last nodes.
+    global alphay
+    f = f(i,:);
+    c = dt/dy;
+    b = (c*f(1:N-2)+2*alphay).*f(1:N-2) + 4*(1-alphay)*f(2:N-1) + (2*alphay-c*f(2:N-1)).*f(3:N);
+    b(1) = b(1) + (c*f(2)+2*alphay)*f(1);
+    b(N-2) = b(N-2) + (2*alphay - f(N-2)*c)*f(N);
+end
+
+
+
