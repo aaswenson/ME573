23c88789c37af4eaf853790233a1154919720b6d
Alexander Swenson <aaswenson@wisc.edu>
2017-12-17 15:29:27 -0600

    almost correct, issue with updating b matrix

--- HW12/advection_diffusion.m
+++ HW12/advection_diffusion.m
@@ -1,5 +1,6 @@
 %% Alex Swenson ME573 HW10, Problem 1
-function [fuCN, fvCN] = advection_diffusion(dt, dx, dy, u, v, nu)
+function [u_2, v_2] = advection_diffusion(dt, dx, dy, u, v, nu,...
+                                           ubot, utop, vleft, vright)
 global alphax alphay
 %% Problem Parameters
 
@@ -7,37 +8,33 @@ nx = length(u(:,1));
 ny = length(v(1,:));
 alphax = nu*dt / dx^2; alphay = nu*dt / dy^2;
 
-fuCN = u;
-fvCN = v;
-fu_CN = u;
-fu_saveCN = u;
-fv_CN = v;    
-fv_saveCN = v;
+u_1 = u; u_2 = u;
+v_1 = v; v_2 = v;
+
 %% March in Time
     for j=2:nx-1
         % ----------------- Crank Nicholson ------------------- %
         % calculate u direction
-        Aux = update_Ax(fuCN, nx, dt, dx, j);
-        bux = update_bx(fuCN, nx, dt, dx, j);
-        fu_CN(2:ny-1,j) = thomas(Aux, bux);
+        Aux = update_Ax(u, nx, dt, dx, j);
+        bux = update_bx(u, u, nx, dt, dx, j, 0, 0);
+        u_1(2:ny-1,j) = thomas(Aux, bux);
         % calculate v direction
-        Avx = update_Ax(fvCN, nx, dt, dx, j);
-        bvx = update_bx(fvCN, nx, dt, dx, j);
-        fv_CN(2:ny-1,j) = thomas(Avx, bvx);
+        Avx = update_Ax(v, nx, dt, dx, j);
+        bvx = update_bx(u, v, nx, dt, dx, j, vleft(j), vright(j));
+        v_1(2:ny-1,j) = thomas(Avx, bvx);
     end
     for i=2:ny-1
         % ----------------- Crank Nicholson ------------------- %
         % calculate u direction
-        Auy = update_Ay(fu_CN, ny, dt, dy, i);
-        buy= update_by(fu_CN, ny, dt, dy, i);
-        fu_saveCN(i,2:nx-1) = thomas(Auy, buy);
+        Auy = update_Ay(u_1, ny, dt, dy, i);
+        buy= update_by(v, u_1, ny, dt, dy, i, ubot(i), utop(i));
+        u_2(i,2:nx-1) = thomas(Auy, buy);
         % calculate v direction
-        Avy = update_Ay(fv_CN, ny, dt, dy, i);
-        bvy= update_by(fv_CN, ny, dt, dy, i);
-        fv_saveCN(i,2:nx-1) = thomas(Avy, bvy);
+        Avy = update_Ay(v_1, ny, dt, dy, i);
+        bvy= update_by(v, v_1, ny, dt, dy, i, 0, 0);
+        v_2(i,2:nx-1) = thomas(Avy, bvy);
     end
-fuCN = fu_saveCN;
-fvCN = fv_saveCN;
+
 
 end
 
@@ -59,26 +56,28 @@ function A = update_Ay(f, N, dt, dx, i)
     A = spdiags([-((f(3:N+1)'*dt/dx) +2*alphay).*e, 4*(1+alphay)*e, (f(1:N-1)'*(dt/dx) - 2*alphay).*e], -1:1, N-2, N-2);
 end
 
-function b = update_bx(f, N, dt, dx, j)
+function b = update_bx(u, f, N, dt, dx, j, bc1, bc2)
     % update the RHS vector, include initial boundary conditions for 
     % first and last nodes.
     global alphax
     f = f(:,j)';
+    u = u(:,j)';
     c = dt/dx;
-    b = (c*f(2:N-1)+2*alphax).*f(1:N-2) + 4*(1-alphax)*f(2:N-1) + (2*alphax-c*f(2:N-1)).*f(3:N);
-    b(1) = b(1) + (c*f(2)+2*alphax)*f(1);
-    b(N-2) = b(N-2) + (2*alphax - f(N-2)*c)*f(N);
+    b = (c*u(2:N-1)+2*alphax).*f(1:N-2) + 4*(1-alphax)*f(2:N-1) + (2*alphax-c*u(2:N-1)).*f(3:N);
+    b(1) = b(1) + (c*u(2)+2*alphax)*bc1;
+%     b(N-2) = b(N-2) + (2*alphax - u(N-2)*c)*bc2;
 end
 
-function b = update_by(f, N, dt, dy, i)
+function b = update_by(v, f, N, dt, dy, i, bc1, bc2)
     % update the RHS vector, include initial boundary conditions for 
     % first and last nodes.
     global alphay
     f = f(i,:);
+    v = v(i,:);
     c = dt/dy;
-    b = (c*f(1:N-2)+2*alphay).*f(1:N-2) + 4*(1-alphay)*f(2:N-1) + (2*alphay-c*f(2:N-1)).*f(3:N);
-    b(1) = b(1) + (c*f(2)+2*alphay)*f(1);
-    b(N-2) = b(N-2) + (2*alphay - f(N-2)*c)*f(N);
+    b = (c*v(1:N-2)+2*alphay).*f(1:N-2) + 4*(1-alphay)*f(2:N-1) + (2*alphay-c*v(2:N-1)).*f(3:N);
+    b(1) = b(1) + (c*v(2)+2*alphay)*bc1;
+%     b(N-2) = b(N-2) + (2*alphay - v(N-2)*c)*bc2;
 end
 
 
--- HW12/problem1.m
+++ HW12/problem1.m
@@ -9,7 +9,7 @@ dx = 0.02; dy=dx; Lx=1.0; Ly=Lx;
 Nx = (Lx/dx) + 1;   Ny = (Ly/dy) + 1;
 save_idx = [int8(0.14/dx), int8(0.08/dy)];
 nu = 0.01; rho = 1.0; dt = 0.1; tfinal = 1; t=0:dt:tfinal;
-nslip = 1; sliptol = 0.00001; vlid = 1.0; 
+nslip = 10; sliptol = 0.00001; vlid = 1.0; 
 
 alphax = nu * dt / dx^2; alphay = nu * dt / dy^2;
 u(Nx,Ny) = 0.0; v(Nx,Ny) = 0.0;
@@ -21,14 +21,15 @@ u(:,Ny) = vlid;
 
 for i=1:tfinal / dt;
     for i=1:nslip;
-        [u_1, v_1] = advection_diffusion(dt, dx, dy, u, v, nu);
+        [u_1, v_1] = advection_diffusion(dt, dx, dy, u, v, nu,...
+                                         ubot', utop', vleft, vright);
         [u_2, v_2] = project_vel(Nx, Ny, u_1, v_1, dx, dy, dt, rho, phi,...
                          ubot', utop', vleft, vright);
         [utop, ubot, vleft, vright, maxslip] =...
             check_vel(u_2, v_2, Nx, Ny, vlid, utop, vleft, ubot, vright);
         if maxslip < sliptol
            disp(['required slip iterations ', num2str(i)])
-           break 
+           break
         end
     end     
      u = u_2;
@@ -49,26 +50,27 @@ end
 
 function [u_2, v_2] = project_vel(Nx, Ny, u, v, dx, dy, dt, rho, phi,...
                                   ubot, utop, vleft, vright)
+    u_2 = u; v_2 = v;
     % compute derivative from converged phi (provided)
     dpx = (phi(3:Nx,2:Ny-1) - phi(1:Ny-2,2:Ny-1)) ./ (2*dx);
     dpy = (phi(2:Nx-1,3:Ny) - phi(2:Nx-1,1:Ny-2)) ./ (2*dy);
     % Lower Boundary Derivatives
-    Li = (phi(3:Nx-2,1) - phi(1:Nx-2,1)) ./ (2*dx);
+    Li = (phi(3:Nx-2,1) - phi(2:Nx-3,1)) ./ (2*dx);
     L0 = 0;
     L2 = (-3*phi(2,1) + 4*phi(3,1) - phi(4,1)) ./ (2*dx);
     LN_1 = (3*phi(Nx-1,1) - 4*phi(Nx-2,1) + phi(Nx-3,1)) ./ (2*dx);
     % Upper Boundary Derivatives
-    Ui = (phi(3:Nx,Ny) - phi(1:Nx-2,Ny)) ./ (2*dx);
+    Ui = (phi(3:Nx-2,Ny) - phi(2:Nx-3,Ny)) ./ (2*dx);
     U0 = 0;
     U2 = (-3*phi(2,Ny) + 4*phi(3,Ny) - phi(4,Ny)) ./ (2*dx);
     UN_1 = (3*phi(Nx-1,Ny) - 4*phi(Nx-2,Ny) + phi(Nx-3,Ny)) ./ (2*dx);
     % LHS Boundary Derivatives
-    LHSi = (phi(1,3:Ny) - phi(1,1:Ny-2)) ./ (2*dy);
+    LHSi = (phi(1,3:Ny-2) - phi(1,2:Ny-3)) ./ (2*dy);
     LHS0 = 0;
     LHS2 = (-3*phi(1,2) + 4*phi(1,3) - phi(1,4)) ./ (2*dy);
     LHSN_1 = (3*phi(1,Ny-1) - 4*phi(1,Ny-2) + phi(1,Ny-3)) ./ (2*dy);
     % RHS Boundary Derivatives
-    RHSi = (phi(Nx,3:Ny) - phi(Nx,1:Ny-2)) ./ (2*dy);
+    RHSi = (phi(Nx,3:Ny-2) - phi(Nx,2:Ny-3)) ./ (2*dy);
     RHS0 = 0;
     RHS2 = (-3*phi(Nx,2) + 4*phi(Nx,3) - phi(Nx,4)) ./ (2*dy);
     RHSN_1 = (3*phi(Nx,Ny-1) - 4*phi(Nx,Ny-2) + phi(Nx,Ny-3)) ./ (2*dy);
@@ -81,16 +83,22 @@ function [u_2, v_2] = project_vel(Nx, Ny, u, v, dx, dy, dt, rho, phi,...
     u_2(3:Nx-2,1) = ubot(3:Nx-2) - (dt/rho).*Li;
     u_2(2,1) = ubot(2) - (dt/rho).*L2;
     u_2(Nx-1,1) = ubot(Nx-1) - (dt/rho).*LN_1;
-    v_2(3:Nx-2,1) = 0;
+    v_2(2:Nx-1,1) = 0;
     % project top nodes
-    u_2(2:Nx-1,Ny) = utop(2:Nx-1) - (dt/rho)*Ui;
-    v_2(2:Nx-1,Ny) = -(dt/rho) * U0;
+    u_2(3:Nx-2,Ny) = utop(3:Nx-2) - (dt/rho).*Ui;
+    u_2(2,Ny) = utop(2) - (dt/rho).*U2;
+    u_2(Nx-1,1) = ubot(Nx-1) - (dt/rho).*UN_1;
+    v_2(2:Nx-1,Ny) = 0;
     % project left nodes
     u_2(1,2:Ny-1) = -(dt/rho)*LHS0;
-    v_2(1,2:Ny-1) = vleft(2:Ny-1) - (dt/rho)*LHSi;
+    v_2(1,3:Ny-2) = vleft(3:Ny-2) - (dt/rho).*LHSi;
+    v_2(1,2) = vleft(2) - (dt/rho).*LHS2;
+    v_2(1,Ny-1) = vleft(Ny-1) - (dt/rho).*LHSN_1;
     % project right nodes
     u_2(Nx,2:Ny-1) = -(dt/rho)*RHS0;
-    v_2(Nx,2:Ny-1) = vright(2:Ny-1) - (dt/rho)*RHSi;
+    v_2(Nx,3:Ny-2) = vright(3:Ny-2) - (dt/rho)*RHSi;
+    v_2(Nx,2) = vleft(2) - (dt/rho).*RHS2;    
+    v_2(Nx,Ny-1) = vleft(Ny-1) - (dt/rho).*RHSN_1;
 end
 
 function [utop, ubot, vleft, vright, maxslip] = check_vel(u, v, Nx, Ny,...
