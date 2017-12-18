8b8496cc6b27ca199951bf74cc0c45128e744b56
Alexander Swenson <aaswenson@wisc.edu>
2017-12-18 13:01:04 -0600

    editing advection diffusion update_(A,b) vectors

--- HW12/advection_diffusion.m
+++ HW12/advection_diffusion.m
@@ -1,7 +1,6 @@
 %% Alex Swenson ME573 HW10, Problem 1
 function [u_2, v_2] = advection_diffusion(dt, dx, dy, u, v, nu,...
                                            ubot, utop, vleft, vright)
-global alphax alphay
 %% Problem Parameters
 
 nx = length(u(:,1));
@@ -11,73 +10,46 @@ alphax = nu*dt / dx^2; alphay = nu*dt / dy^2;
 u_1 = u; u_2 = u;
 v_1 = v; v_2 = v;
 
+Cox = u.*(dt/dx); Coy = v.*(dt/dy);
 %% March in Time
     for j=2:nx-1
         % ----------------- Crank Nicholson ------------------- %
         % calculate u direction
-        Aux = update_Ax(u, nx, dt, dx, j);
-        bux = update_bx(u, u, nx, dt, dx, j, 0, 0);
-        u_1(2:ny-1,j) = thomas(Aux, bux);
+        Ax = update_A(Cox(:,j)', nx, alphax);
+        bux = update_b(Cox(:,j)', u(:,j)', nx, alphax, 0, 0);
+        u_1(2:ny-1,j) = thomas(Ax, bux);
         % calculate v direction
-        Avx = update_Ax(v, nx, dt, dx, j);
-        bvx = update_bx(u, v, nx, dt, dx, j, vleft(j), vright(j));
-        v_1(2:ny-1,j) = thomas(Avx, bvx);
+        bvx = update_b(Cox(:,j)', v(:,j)', nx, alphax, vleft(j), vright(j));
+        v_1(2:ny-1,j) = thomas(Ax, bvx);
     end
     for i=2:ny-1
         % ----------------- Crank Nicholson ------------------- %
         % calculate u direction
-        Auy = update_Ay(u_1, ny, dt, dy, i);
-        buy= update_by(v, u_1, ny, dt, dy, i, ubot(i), utop(i));
-        u_2(i,2:nx-1) = thomas(Auy, buy);
+        Ay = update_A(Coy(:,j)', ny, alphay);
+        buy= update_b(Coy(i,:), u_1(i,:), ny, alphay, ubot(i), utop(i));
+        u_2(i,2:nx-1) = thomas(Ay, buy);
         % calculate v direction
-        Avy = update_Ay(v_1, ny, dt, dy, i);
-        bvy= update_by(v, v_1, ny, dt, dy, i, 0, 0);
-        v_2(i,2:nx-1) = thomas(Avy, bvy);
+        bvy= update_b(Coy(i,:), v_1(i,:), ny, alphay, 0, 0);
+        v_2(i,2:nx-1) = thomas(Ay, bvy);
     end
 
 
 end
 
-function A = update_Ax(f, N, dt, dx, j)
-    global alphax
-    f = f(:,j)';
+function A = update_A(f, N, alpha)
     e = ones(N-1, 1);
     % set ghost point to make matrix
     f(N+1) = 1;
-    A = spdiags([-((f(3:N+1)'*dt/dx) +2*alphax).*e, 4*(1+alphax)*e, (f(1:N-1)'*(dt/dx) - 2*alphax).*e], -1:1, N-2, N-2);
+    A = spdiags([-(f(3:N+1)' +2*alpha).*e, 4*(1+alpha)*e, (f(1:N-1)' - 2*alpha).*e], -1:1, N-2, N-2);
 end
 
-function A = update_Ay(f, N, dt, dx, i)
-    global alphay
-    f = f(i,:);
-    e = ones(N-1, 1);
-    % set ghost point to make matrix
-    f(N+1) = 1;
-    A = spdiags([-((f(3:N+1)'*dt/dx) +2*alphay).*e, 4*(1+alphay)*e, (f(1:N-1)'*(dt/dx) - 2*alphay).*e], -1:1, N-2, N-2);
-end
 
-function b = update_bx(u, f, N, dt, dx, j, bc1, bc2)
+function b = update_b(Co, f, N, alpha, bc1, bc2)
     % update the RHS vector, include initial boundary conditions for 
     % first and last nodes.
-    global alphax
-    f = f(:,j)';
-    u = u(:,j)';
-    c = dt/dx;
-    b = (c*u(2:N-1)+2*alphax).*f(1:N-2) + 4*(1-alphax)*f(2:N-1) + (2*alphax-c*u(2:N-1)).*f(3:N);
-    b(1) = b(1) + (c*u(2)+2*alphax)*bc1;
-    b(N-2) = b(N-2) + (2*alphax - u(N-2)*c)*bc2;
-end
-
-function b = update_by(v, f, N, dt, dy, i, bc1, bc2)
-    % update the RHS vector, include initial boundary conditions for 
-    % first and last nodes.
-    global alphay
-    f = f(i,:);
-    v = v(i,:);
-    c = dt/dy;
-    b = (c*v(1:N-2)+2*alphay).*f(1:N-2) + 4*(1-alphay)*f(2:N-1) + (2*alphay-c*v(2:N-1)).*f(3:N);
-    b(1) = b(1) + (c*v(2)+2*alphay)*bc1;
-    b(N-2) = b(N-2) + (2*alphay - v(N-2)*c)*bc2;
+    b = (Co(2:N-1)+2*alpha).*f(1:N-2) + 4*(1-alpha)*f(2:N-1) + (2*alpha-Co(2:N-1)).*f(3:N);
+    b(1) = b(1) + (Co(2)+2*alpha)*bc1;
+    b(N-2) = b(N-2) + (2*alpha - Co(N-2))*bc2;
 end
 
 
